7d518789a4f0dcb6e85e301ddc9b0e86aaf46777
zachariahstauffer <stauffer.zachariah2222@gmail.com>
2025-10-30 21:41:57 -0500

    added password requirements

--- Main.py
+++ Main.py
@@ -12,8 +12,8 @@ class data:
     def read(self):
         try:
 
-            with open('data.dat', 'rb') as file:
-                self.users = pickle.load(file)
+            file = open('data.dat', 'rb')
+            self.users = pickle.load(file)
 
         except EOFError:
             self.users = []
@@ -25,7 +25,6 @@ class txt_to_hash:
         self.tx = tx
         self.s = bcrypt.gensalt()
         self.en = tx.encode('utf-8')
-        self.has = self.hashh()
     
     def hashh(self):
 
@@ -34,27 +33,79 @@ class txt_to_hash:
 class user:
     def __init__(self, username: str, password):
         self.username = username
-        self.password = password
         self.has = b''
-        self.hass()
+        self.hass(password)
     
     def get_username(self):
         return self.username
     
-    def hass(self):
-        self.has = txt_to_hash(self.password)
+    def hass(self, password):
+        self.has = txt_to_hash(password).hashh()
 
     def get_has(self):
         return self.has
     
 class verify:
-    def __init__(self, password, has):
+    def __init__(self, password):
         self.password = password
-        self.has = has.has
 
-    def vf(self):
+    def vs(self):
+        val = True
+        has_upper = has_lower = has_special = has_number = False
 
-        return bool(bcrypt.checkpw(self.password, self.has))
+        if len(self.password) < 6:
+            print('password must be 6 characters long')
+            val = False
+
+        if len(self.password) > 20:
+            print('password can not be more than 20 characters long')
+            val = False
+        
+        for char in self.password:
+            has_upper, has_lower, has_special, has_number = self.requirements(char, has_upper, has_lower, has_special, has_number)
+
+        if has_upper and has_lower and has_special and has_upper:
+            return val
+        
+        if not has_upper:
+            print('password must contain at least one uppercase letter')
+            val = False
+
+        if not has_lower:
+            print('password must contain at least one lowercase letter')
+            val = False
+
+        if not has_number:
+            print('password must contain at least one number')
+            val = False
+
+        if not has_special:
+            print("password must contain at least one special character ['!' , '@', '#', '$', '%', '^', '&']")
+            val = False
+
+
+        return val
+
+    def requirements(self, char, has_upper, has_lower, has_special, has_number):
+        special = {'!' , '@', "#", '$', '%', '^', '&'}
+
+        if 48 <= ord(char) <= 57:
+            has_number = True
+            
+        if 65 <= ord(char) <= 90:
+            has_upper = True
+
+        if 97 <= ord(char) <= 122:
+            has_lower = True
+
+        if char in special:
+            has_special = True
+
+        return has_upper, has_lower, has_special, has_number
+
+    def vl(self, has):
+
+        return bool(bcrypt.checkpw(self.password, has))
     
 class display:
     def __init__(self):
@@ -68,26 +119,36 @@ class display:
 
         new_user = user(username, password)
 
-        self.users.append(new_user)
-
+        if verify(password).vs():
+            self.users.append(new_user)
+        
         self.data.save(self.users)
 
     def login(self):
         username = str(input("Username: "))
         password = str(input("Password: ")).encode('utf-8')
+        
+        print('looking for user')
     
         for user in self.users or []:
 
-            print('looking for user')
-
-            if str(user.get_username()) == username:
+            if str(user.get_username()) != username:
+                continue
    
-                print('user found, checking password')
-                has = user.get_has()
+            print('user found, checking password')
+            has = user.get_has()
 
-                if verify(password, has).vf():
-                    print(f"{username} has logged in")
+            if verify(password).vl(has):
+                print(f"{username} has logged in")
                 break
+
+            print('wrong password')
+            break
+
+        else:
+            print('user doesn not have an account')
+
+
 
     def check_data(self):
         self.users = self.data.read()
@@ -99,13 +160,13 @@ class run:
     def run(self):
         choice = str(input("sign-up or login: "))
 
-        if choice == 'sign-up':
+        if choice == 'sign-up' or choice == 's':
             display().sign_up()
 
-        elif choice == 'login':
+        elif choice == 'login' or choice == 'l':
             display().login()
 
-        elif choice == 'wipe':
+        elif choice == 'wipe' or choice == 'w':
             with open('data.dat', 'wb') as file:
                 pickle.dump([], file)
             
