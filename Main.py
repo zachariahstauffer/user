4b07dad7020840294aecdda706213769bf5a4404
zachariahstauffer <100742502+zachariahstauffer@users.noreply.github.com>
2025-11-04 18:39:15 +0000

    small edits

--- Main.py
+++ Main.py
@@ -1,12 +1,28 @@
 import Data
 import User
 import Verify
+# import concurrent.futures
     
 class Display:
     def __init__(self):
         self.users = []
         self.data = Data.Data()
         self.check_data()
+
+    def run(self):
+        choice = str(input("sign-up or login: "))
+
+        if choice == 'sign-up' or choice == 's':
+            self.sign_up()
+
+        elif choice == 'login' or choice == 'l':
+            self.login()
+
+        elif choice == 'wipe' or choice == 'w':
+            Data.Data().wipe()
+            
+        else:
+            print('error')
 
     def sign_up(self):
         username = str(input("Make Username: "))
@@ -43,29 +59,8 @@ class Display:
         else:
             print('user doesn not have an account')
 
-
-
     def check_data(self):
         self.users = self.data.read()
-
-class Run:
-    def __init__(self):
-        pass
-
-    def run(self):
-        choice = str(input("sign-up or login: "))
-
-        if choice == 'sign-up' or choice == 's':
-            Display().sign_up()
-
-        elif choice == 'login' or choice == 'l':
-            Display().login()
-
-        elif choice == 'wipe' or choice == 'w':
-            Data.Data().wipe()
-            
-        else:
-            print('error')
-
+   
 if __name__ == '__main__':
-    Run().run()
+    Display().run()
--- Txt_to_hash.py
+++ Txt_to_hash.py
@@ -1,11 +1,11 @@
 import bcrypt
 
 class Txt_to_hash:
-    def __init__(self, tx: str):
-        self.tx = tx
-        self.s = bcrypt.gensalt()
-        self.en = tx.encode('utf-8')
+    def __init__(self, text: str):
+        self.text = text
+        self.salt = bcrypt.gensalt()
+        self.encode = text.encode('utf-8')
     
     def hashh(self):
 
-        return bcrypt.hashpw(self.en, self.s)
+        return bcrypt.hashpw(self.encode, self.salt)
